673bb08b15ad6d7ad47cac7532078c8ba079324d
obaranov <obaranov@redhat.com>
2016-11-18 12:13:26 +0200

    Provisioner ovb support
    Intial implementation.
    
    Change-Id: I160fc1688fc1afaf27d488cd2ed9645e2bfc780e

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
 
 from infrared import api
-from infrared.core.plugins import InfraredPlugin
-
+from infrared.core.services.plugins import InfraredPlugin
+from infrared.main import ProfileManagerSpec
 
 def main():
     specs_manager = api.SpecManager()
     plugin = InfraredPlugin.from_config_file(os.getcwd(), 'infrared.cfg')
     specs_manager.register_spec(api.DefaultInfraredPluginSpec(plugin))
+    specs_manager.register_spec(ProfileManagerSpec(
+        'profile', description="Profile manager. " \
+        "Allows to create and use an isolated environement " \
+        "for plugins execution."))
     specs_manager.run_specs()
 
 if __name__ == '__main__':
